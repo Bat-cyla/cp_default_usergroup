dc4d270be3a7d96c2c4f2a4cf07dea5935efa26c
Bat-cyla <cp.nemov@gmail.com>
2024-08-06 16:21:56 +0400

    правка убрана распечатка,исправлена версия модуля

--- app/addons/cp_default_usergroup/controllers/backend/addons.pre.php
+++ app/addons/cp_default_usergroup/controllers/backend/addons.pre.php
@@ -11,7 +11,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     );
 
     if ($mode == 'update') {
-        fn_print_r($_REQUEST);
         if (isset($_REQUEST['addon_data']) && $_REQUEST['addon'] == 'cp_default_usergroup') {
             foreach($_REQUEST['addon_data']['options'] as $object_id => $val) {
 
